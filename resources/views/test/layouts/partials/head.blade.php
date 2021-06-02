840f73a82a243007d3df044a5f6bddc173986887
Berat <berat.niziplioglu@wicasa.com.tr>
2021-06-02 22:33:50 +0300

    Fixed bootstrap.js

--- resources/views/test/layouts/partials/head.blade.php
+++ resources/views/test/layouts/partials/head.blade.php
@@ -4,8 +4,5 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
